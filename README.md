f2de47c80adc6daa81be8bf4996f655847972c31
docyx <oliverxur@gmail.com>
2022-12-11 22:22:36 -0500

    add index to readme, husky

--- README.md
+++ README.md
@@ -12,6 +12,22 @@ Now, in your terminal (example usage):
 YEAR=2022 DAY=4 npm run start
 ```
 
+## Index
+
+<!-- __INJECT_INDEX__ -->
+- 2022
+  - [Day 1](./2022/1.ts)
+  - [Day 2](./2022/2.ts)
+  - [Day 3](./2022/3.ts)
+  - [Day 4](./2022/4.ts)
+  - [Day 5](./2022/5.ts)
+  - [Day 6](./2022/6.ts)
+  - [Day 7](./2022/7.ts)
+  - [Day 8](./2022/8.ts)
+  - [Day 10](./2022/10.ts)
+  - [Day 11](./2022/11.ts)
+<!-- /__INJECT_INDEX__ -->
+
 ## License
 
 [MIT](./LICENSE)
---
+++ scripts/inject-index.ts
@@ -0,0 +1,56 @@
+import { rmSync } from 'fs'
+import { readdir, readFile, writeFile } from 'fs/promises'
+import { createInterface } from 'readline/promises'
+
+const thisDir = await readdir('.', { withFileTypes: true })
+
+const yearDirs = thisDir
+  .filter((x) => x.isDirectory() && /[0-9]{4}/.test(x.name))
+  .sort((a, b) => parseInt(b.name) - parseInt(a.name))
+
+let injected = ''
+
+for (const [i, year] of yearDirs.entries()) {
+  injected += `- ${year.name}\n`
+
+  const days = (await readdir(year.name))
+    .map((d) => parseInt(d.replace('.ts', '')))
+    .sort((a, b) => a - b)
+
+  for (const day of days) {
+    injected += `  - [Day ${day}](./${year.name}/${day}.ts)\n`
+  }
+
+  if (i < yearDirs.length - 1) injected += '\n'
+}
+
+const OPEN_INJECT = '<!-- __INJECT_INDEX__ -->'
+const CLOSE_INJECT = '<!-- /__INJECT_INDEX__ -->'
+
+const dirtyReadme = (await readFile('README.md')).toString()
+
+const start = dirtyReadme.indexOf(OPEN_INJECT)
+const end = dirtyReadme.indexOf(CLOSE_INJECT)
+
+let newReadme = dirtyReadme.slice(0, start)
+
+newReadme += `${OPEN_INJECT}\n${injected}${CLOSE_INJECT}`
+newReadme += dirtyReadme.slice(end + CLOSE_INJECT.length)
+
+await writeFile('README.staging.md', newReadme)
+
+const cleanUp = () => rmSync('README.staging.md')
+
+process.on('exit', () => cleanUp())
+
+const rl = createInterface({ input: process.stdin, output: process.stdout })
+
+const acceptStage = await rl.question(
+  'View ./README.staging.md, enter [y] to accept: '
+)
+
+if (acceptStage === '' || acceptStage.toLowerCase() === 'y') {
+  writeFile('README.md', newReadme)
+}
+
+rl.close()
